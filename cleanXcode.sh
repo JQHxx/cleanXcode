9cfe9ffcdf317b5197d8d2cf82f1a4ba09a90e40
JQHxx <1071171406@qq.com>
2022-09-30 16:20:03 +0800

    Update cleanXcode.sh

--- cleanXcode.sh
+++ cleanXcode.sh
@@ -11,9 +11,13 @@ echo
     exit 1
 fi
 echo 
-echo "开始清除DerivedData..."
+echo "开始清除DerivedData、iOS DeviceSupport、 iOS Device Logs、CoreSimulator Caches DocumentationCache..."
 rm -rf ~/Library/Developer/Xcode/DerivedData
-echo "DerivedData已清除"
+rm -rf ~/Library/Developer/Xcode/iOS\ DeviceSupport/
+rm -rf ~/Library/Developer/Xcode/iOS\ Device\ Logs/
+rm -rf ~/Library/Developer/CoreSimulator/Caches/
+rm -rf ~/Library/Developer/Xcode/DocumentationCache/
+echo "DerivedData、iOS DeviceSupport、 iOS Device Logs、CoreSimulator Caches DocumentationCache已清除"
 echo 
 read -p "是否要删除Archives，Xcode打包文件 [yes/NO] " -n 1 -r
 if [[  $REPLY =~ ^[Yy]$ ]]
@@ -29,9 +33,9 @@ if [[  $REPLY =~ ^[Yy]$ ]]
 then
 echo 
 echo "开始清除运行xcode后在模拟器上安装的app..."
-rm -rf ~/Library/Developer/CoreSimulator/Devices
+# 模拟器如果移除，需要重新添加
+# rm -rf ~/Library/Developer/CoreSimulator/Devices
 echo "模拟器上安装的app已清除"
 echo "CoreSimulator文件夹删除后请重启Xcode"
 fi
 echo ""
-
